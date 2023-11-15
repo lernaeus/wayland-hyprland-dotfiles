24656fc86300e1f13143ecb716213352b9b54974
lernaeus-ton <damnedhoopa@gmail.com>
2023-11-15 21:59:36 +0200

    some scripts

---
+++ useful-scripts/set-gtk-theme.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+gsettings set org.gnome.desktop.interface gtk-theme Gruvbox-Material-Dark
+gsettings set org.gnome.desktop.interface icon-theme Gruvbox-Material-Dark
+gsettings set org.gnome.desktop.interface cursor-theme Capitaine-Gruvbox
+gsettings set org.gnome.desktop.interface font-name 'Inter 12'
+
