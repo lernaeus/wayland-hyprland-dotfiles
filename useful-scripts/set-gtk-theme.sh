854c32f30bdd664a729be2812f44efa4d47d23e7
lernaeus-ton <114980450+lernaeus-ton@users.noreply.github.com>
2024-04-24 23:39:25 +0300

    more small stuffs

--- useful-scripts/set-gtk-theme.sh
+++ useful-scripts/set-gtk-theme.sh
@@ -4,4 +4,4 @@ gsettings set org.gnome.desktop.interface gtk-theme Gruvbox-Material-Dark
 gsettings set org.gnome.desktop.interface icon-theme Gruvbox-Material-Dark
 gsettings set org.gnome.desktop.interface cursor-theme Capitaine-Gruvbox
 gsettings set org.gnome.desktop.interface font-name 'Inter 12'
-
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
