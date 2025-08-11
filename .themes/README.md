f8d035ddad2dea5261c92301045810cbc21419cf
lernaeus <114980450+lernaeus@users.noreply.github.com>
2025-08-11 03:08:38 +0300

    even more changes

--- .themes/README.md
+++
@@ -1,6 +0,0 @@
-***Sources***
-
-
-
-- GTK 2 and 3 theme (4 is kinda broken, looking for GTK 4 replacer): [https://github.com/TheGreatMcPain/gruvbox-material-gtk]()
-- Discord: [https://github.com/shvedes/dotfiles]()
--- useful-scripts/set-gtk-theme.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-gsettings set org.gnome.desktop.interface gtk-theme Gruvbox-Material-Dark
-gsettings set org.gnome.desktop.interface icon-theme Gruvbox-Material-Dark
-gsettings set org.gnome.desktop.interface cursor-theme Capitaine-Gruvbox
-gsettings set org.gnome.desktop.interface font-name 'Inter 12'
-gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
